a59bd4abd29c3a54bd51e2d202ee611a686cd552
nacardozog <nacardozog@heon.com.co>
2021-04-18 11:35:38 -0500

    cargue inicial del proyecto, se sube a git

---
+++ AppVentas/AppAlertas/Controllers/ComunicacionController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AppVentas.Controllers
+{
+    public class ComunicacionController : ApiController
+    {
+
+        public class objetoinsertClientes
+        {
+            public string numeroDocumento;
+            public string completosNombre;
+            public string direccion;
+            public string telefono;
+
+        }
+        public class objetoinsertProductos
+        {
+            public string nombre;
+            public string precioActual;
+            public int stock;
+            public int proveedor;
+
+        }
+        public class objetoDelClientes
+        {
+            public string numeroDocumento;
+
+        }
+        public class objetoinsertrProvedores
+        {
+            public string RUT;
+            public string nombre;
+            public string direccion;
+            public string paginaWeb;
+        }
+
+        public class objetoinsertaVentas
+        {
+            public int idcliente;
+            public int descuento;
+            public float montoFinal;
+            public string paginaWeb;
+        }
+        public class objetoinsertaVentaDetalles
+        {
+            public int idVenta;
+            public int idproducto;
+        }
+        public class objetoUpdVentas
+        {       
+            public string numeroDocumento;
+            public string nombre;
+            public string direccion;
+            public string telefono;
+            public int idusuario;
+        }
+
+
+
+       
+        [Route("insertarClientes")]
+        [HttpPost]
+        public IHttpActionResult insertarClientes([FromBody]objetoinsertClientes dato)
+        {
+            return Ok(lnVentas.Conexion.ConexionBD("exec SVentas.Add_cliente  @numeroDocumento=N'" + dato.numeroDocumento + "',@completosNombre=N'" + dato.completosNombre + "',@direccion=N'" + dato.direccion + "',@telefono=N'" + dato.telefono + "'  "));
+        }
+        [Route("insertarProductos")]
+        [HttpPost]
+        public IHttpActionResult insertarProductos([FromBody]objetoinsertProductos dato)
+        {
+            return Ok(lnVentas.Conexion.ConexionBD("exec SVentas.Add_Productos  @nombre=N'" + dato.nombre + "',@precioActual=N'" + dato.precioActual + "',@stock=N'" + dato.stock + "',@proveedor=N'" + dato.proveedor + "'  "));
+        }
+        [Route("insertarProvedores")]
+        [HttpPost]
+        public IHttpActionResult insertarProvedores([FromBody]objetoinsertrProvedores dato)
+        {
+            return Ok(lnVentas.Conexion.ConexionBD("exec SVentas.Add_Provedores  @RUT=N'" + dato.RUT + "',@nombre=N'" + dato.nombre + "',@direccion=N'" + dato.direccion + "',@paginaWeb=N'" + dato.paginaWeb + "'  "));
+        }
+        [Route("insertarVentas")]
+        [HttpPost]
+        public IHttpActionResult insertarVentas([FromBody]objetoinsertaVentas dato)
+        {
+            return Ok(lnVentas.Conexion.ConexionBD("exec SVentas.Add_Ventas  @idcliente=N'" + dato.idcliente + "',@descuento=N'" + dato.descuento + "',@montoFinal=N'" + dato.montoFinal + "'"));
+        }
+
+        [Route("insertarVentasDetalle")]
+        [HttpPost]
+        public IHttpActionResult insertarVentasDetalle([FromBody]objetoinsertaVentaDetalles dato)
+        {
+            return Ok(lnVentas.Conexion.ConexionBD("exec SVentas.Add_VentasDetalle  @idVenta=N'" + dato.idVenta + "',@idproducto=N'" + dato.idproducto + "'"));
+        }
+
+        [Route("ActualizaClientes")]
+        [HttpPost]
+        public IHttpActionResult ActualizaClientes([FromBody]objetoUpdVentas dato)
+        {
+            return Ok(lnVentas.Conexion.ConexionBD("exec SVentas.Upd_cliente  @numeroDocumento=N'" + dato.numeroDocumento + "',@nombre=N'" + dato.nombre + "',@direccion=N'" + dato.direccion + "',@telefono=N'" + dato.telefono + "',@idusuario=N'" + dato.idusuario + "'   "));
+        }
+
+        [Route("EliminaClientes")]
+        [HttpPost]
+        public IHttpActionResult EliminaClientes([FromBody]objetoDelClientes dato)
+        {
+            return Ok(lnVentas.Conexion.ConexionBD("exec SVentas.Del_cliente  @numeroDocumento=N'" + dato.numeroDocumento + "' "));
+        }
+
+        [Route("listaVentas")]
+        [HttpGet]
+        public IHttpActionResult listaVentas()
+        {
+            return Ok(lnVentas.Conexion.ConexionBD("exec  SVentas.Lst_Ventas "));
+        }
+
+    }
+}
---
+++ AppVentas/lnAppAlerta/Conexion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lnVentas
+{
+    public class Conexion
+    {
+        public static DataTable ConexionBD(string sentencia )
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString =
+            "Data Source=localhost;" +
+            "Initial Catalog=ventasComercio;" +
+            "User id=Natalia;" +
+            "Password=123456789;";
+            using (SqlConnection con = new SqlConnection(conn.ConnectionString))
+            {
+                if (con.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                   
+                }
+            }
+            SqlCommand comando = new SqlCommand(sentencia, conn);
+            SqlDataReader registro = comando.ExecuteReader();
+
+            var tb = new DataTable();
+            tb.Load(registro);
+
+            conn.Close();
+            return tb;
+        }
+    }
+}
